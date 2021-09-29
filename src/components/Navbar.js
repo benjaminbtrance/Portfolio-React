997048ae7b4840bd59d77374b4600b4b5c436d4a
benjaminbtrance <benjaminb.tran@gmail.com>
2021-09-28 19:44:36 -0500

    add reactstrap nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	Container,
+} from 'reactstrap';
 
-const Navbar = () => {
+const ProfolioNav = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-dark">
-			<div className="container">
-				<a className="navbar-brand logo" href="#">
-					BBT
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav ms-auto">
-						<li className="nav-item active">
-							<a className="nav-link" href="#">
-								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								About Me
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Portfolio
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Contacts
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<Navbar className="navbar navbar-expand-lg navbar-light bg-dark">
+			<Container>
+				<NavbarBrand className="logo">BBT</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="navbar-nav ms-auto" navbar>
+						<NavItem>
+							<NavLink href="#">Home</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="#">About Me</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="#">Portfolio</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="#">Contacts</NavLink>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Container>
+		</Navbar>
 	);
 };
 
-export default Navbar;
+export default ProfolioNav;
